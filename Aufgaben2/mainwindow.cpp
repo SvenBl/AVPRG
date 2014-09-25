0abd0fb937b909af7d3c0ec31630ccff4500989a
SvenBl <sven.blanck@haw-hamburg.de>
2014-09-25 20:31:27 +0200

    Aufgaben 2 nach neuem Zettel

--- Aufgaben2/mainwindow.cpp
+++ Aufgaben2/mainwindow.cpp
@@ -7,6 +7,11 @@
 #include "cvmattoqimage.h"
 
 using namespace cv;
+Mat picture;
+Mat picture2;
+double shift;
+double scale=1;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -20,17 +25,29 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionZweites_Bild_triggered(){
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "",  tr("Images (*.png *.xpm *.jpg)"));
+    Mat image = imread(fileName.toStdString());
+
+    Mat resizedImage;
+    Size resizedImageSize(picture.cols,picture.rows);
+    cv::resize(image,resizedImage, resizedImageSize,0,0);
+    resizedImage.copyTo(picture2);
+
+}
+
 void MainWindow::on_action_ffnen_triggered()
 {
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "",  tr("Images (*.png *.xpm *.jpg)"));
     Mat image = imread(fileName.toStdString());
 
-
+    image.copyTo(picture);
 
     //Konsolenausgabe
     int width = image.cols;
     int height = image.rows;
-    std::cout << "Aufgabe 1:\nWidth: "<< width<< " Height: "<< height << " Channels: " << image.channels() << " Depth: " << image.depth() << " | " <<endl;
+    std::cout << "Aufgabe 1:\nWidth: "<< width<< " Height: "<< height << " Channels: " << image.channels() << " Depth: " << image.depth() << " | ";
     Vec3b pixel1 = image.at<Vec3b>(0,0);
     Vec3b pixel2 = image.at<Vec3b>(1,0);
     Vec3b pixel3 = image.at<Vec3b>((height-1),0);
@@ -38,35 +55,63 @@ void MainWindow::on_action_ffnen_triggered()
     std::cout <<"\nAufgabe 2:\nTopRight: Red: " << (int)pixel1[2] << " Green: " <<  (int)pixel1[1] << " Blue: " << (int)pixel1[0] <<
         " \n2ndRow: Red: " << (int)pixel2[2] << " Green: " <<  (int)pixel2[1] << " Blue: " << (int)pixel2[0] <<
         " \nBottomLeft: Red: " << (int)pixel3[2] << " Green: " <<  (int)pixel3[1] << " Blue: " << (int)pixel3[0] <<
-        " \nCenter: Red: " << (int)pixel4[2] << " Green: " <<  (int)pixel4[1] << " Blue: " << (int)pixel4[0] << "\n";
+        " \nCenter: Red: " << (int)pixel4[2] << " Green: " <<  (int)pixel4[1] << " Blue: " << (int)pixel4[0] << "\n" <<endl;
 
 
-    //+10
-    Mat hell;
-    image.copyTo(hell);
-    for(int x = 0; x < width; x++){
-        for(int y = 0; y < height; y++){
-            hell.at<Vec3b>(y,x)[0] = image.at<Vec3b>(y,x)[0] + 10;
-            hell.at<Vec3b>(y,x)[1] = image.at<Vec3b>(y,x)[1] + 10;
-            hell.at<Vec3b>(y,x)[2] = image.at<Vec3b>(y,x)[2] + 10;
-        }
-    }
+    ui->imageLabel1->setPixmap(cvMatToQPixmap(rescale(picture)));
+}
+
+
+void MainWindow::on_actionHelligkeit_Kontrast_triggered(){
+
+    ui->imageLabel1->setPixmap(cvMatToQPixmap(rescale(picture)));
+    ui->imageLabel2->setPixmap(cvMatToQPixmap(rescale(picture)));
+
+
+}
+
+void MainWindow::on_actionGraustufen_Bin_r_triggered(){
 
     //Graustufen
-    Mat grey(height, width, CV_8UC1);
-    for(int x = 0; x < width; x++){
-        for(int y = 0; y < height; y++){
-            int red = image.at<Vec3b>(y,x)[2];
-            int green = image.at<Vec3b>(y,x)[1];
-            int blue = image.at<Vec3b>(y,x)[0];
+    Mat grey(picture.rows, picture.cols, CV_8UC1);
+    for(int x = 0; x < picture.cols; x++){
+        for(int y = 0; y < picture.rows; y++){
+            int red = picture.at<Vec3b>(y,x)[2];
+            int green = picture.at<Vec3b>(y,x)[1];
+            int blue = picture.at<Vec3b>(y,x)[0];
             grey.at<uchar>(y,x) = (red + green + blue) /3;
         }
     }
 
+    Mat binary(picture.rows, picture.cols, CV_THRESH_BINARY);
+    for(int x = 0; x < picture.cols; x++){
+        for(int y = 0; y < picture.rows; y++){
+            int red = picture.at<Vec3b>(y,x)[2];
+            int green = picture.at<Vec3b>(y,x)[1];
+            int blue = picture.at<Vec3b>(y,x)[0];
+            if(((red + green + blue) /3) >127){
+                binary.at<uchar>(y,x) = 255;
+            }
+            else{
+                binary.at<uchar>(y,x) = 0;
+            }
+        }
+    }
+
+
+    ui->imageLabel1->setPixmap(cvMatToQPixmap(rescale(picture)));
+    ui->imageLabel2->setPixmap(cvMatToQPixmap(rescale(grey)));
+    ui->imageLabel3->setPixmap(cvMatToQPixmap(rescale(binary)));
+}
+
+void MainWindow::on_actionRGB_Komponenten_triggered(){
+
+    int height = picture.rows;
+    int width = picture.cols;
     Mat redgrey(height, width, CV_8UC1);
     for(int x = 0; x < width; x++){
         for(int y = 0; y < height; y++){
-            int red = image.at<Vec3b>(y,x)[2];
+            int red = picture.at<Vec3b>(y,x)[2];
             redgrey.at<uchar>(y,x) = red;
         }
     }
@@ -74,7 +119,7 @@ void MainWindow::on_action_ffnen_triggered()
     Mat bluegrey(height, width, CV_8UC1);
     for(int x = 0; x < width; x++){
         for(int y = 0; y < height; y++){
-            int blue = image.at<Vec3b>(y,x)[0];
+            int blue = picture.at<Vec3b>(y,x)[0];
             bluegrey.at<uchar>(y,x) = blue;
         }
     }
@@ -82,20 +127,29 @@ void MainWindow::on_action_ffnen_triggered()
     Mat greengrey(height, width, CV_8UC1);
     for(int x = 0; x < width; x++){
         for(int y = 0; y < height; y++){
-            int green = image.at<Vec3b>(y,x)[1];
+            int green = picture.at<Vec3b>(y,x)[1];
             greengrey.at<uchar>(y,x) = green;
         }
     }
 
+    ui->imageLabel1->setPixmap(cvMatToQPixmap(rescale(picture)));
+    ui->imageLabel2->setPixmap(cvMatToQPixmap(rescale(redgrey)));
+    ui->imageLabel3->setPixmap(cvMatToQPixmap(rescale(greengrey)));
+    ui->imageLabel4->setPixmap(cvMatToQPixmap(rescale(bluegrey)));
+}
 
 
-    ui->imageLabel->setPixmap(cvMatToQPixmap(rescale(redgrey)));
+void MainWindow::on_actionBlendfunktion_triggered(){
+
+    ui->imageLabel1->setPixmap(cvMatToQPixmap(rescale(picture)));
+    ui->imageLabel2->setPixmap(cvMatToQPixmap(rescale(picture2)));
+    ui->imageLabel3->setPixmap(cvMatToQPixmap(rescale(picture)));
 }
 
 Mat MainWindow::rescale(Mat &mat){
     // Skalierungsfaktor bestimmen
-    float labelWidth = ui->imageLabel->width();
-    float labelHeight = ui->imageLabel->height();
+    float labelWidth = ui->imageLabel1->width();
+    float labelHeight = ui->imageLabel1->height();
     float imageWidth = mat.cols;
     float imageHeight = mat.rows;
     float scale = labelWidth / imageWidth;
@@ -111,4 +165,85 @@ Mat MainWindow::rescale(Mat &mat){
     Mat scaledImage (scaledImageHeight, scaledImageWidth, mat.type());
     cv::resize(mat, scaledImage, Size(scaledImageWidth, scaledImageHeight));
     return scaledImage;
+}
+
+void MainWindow::on_horizontalSlider_valueChanged(int value)
+{
+    scale = (double)value / 100;
+    Mat contrast;
+    picture.copyTo(contrast);
+    for(int x = 0; x < picture.cols; x++){
+        for(int y = 0; y < picture.rows; y++){
+            contrast.at<Vec3b>(y,x)[0] = picture.at<Vec3b>(y,x)[0] * scale + shift;
+            contrast.at<Vec3b>(y,x)[1] = picture.at<Vec3b>(y,x)[1] * scale + shift;
+            contrast.at<Vec3b>(y,x)[2] = picture.at<Vec3b>(y,x)[2] * scale + shift;
+        }
+    }
+    ui->imageLabel2->setPixmap(cvMatToQPixmap(rescale(contrast)));
+}
+
+
+
+void MainWindow::on_horizontalSlider_3_valueChanged(int value)
+{
+    Mat blend;
+    picture.copyTo(blend);
+    for(int x = 0; x < picture.cols; x++){
+        for(int y = 0; y < picture.rows; y++){
+            double blue1 = picture.at<Vec3b>(y,x)[0];
+            double blue2 = picture2.at<Vec3b>(y,x)[0];
+            double green1 = picture.at<Vec3b>(y,x)[1];
+            double green2 = picture2.at<Vec3b>(y,x)[1];
+            double red1 = picture.at<Vec3b>(y,x)[2];
+            double red2 = picture2.at<Vec3b>(y,x)[2];
+
+            double reddiff = red1 - red2;
+            double greendiff = green1 - green2;
+            double bluediff = blue1 - blue2;
+
+            double rednew = red1 -(reddiff /100 * value);
+            double greennew = green1 -(greendiff /100 * value);
+            double bluenew = blue1 -(bluediff /100 * value);
+
+            blend.at<Vec3b>(y,x)[0] = bluenew;
+            blend.at<Vec3b>(y,x)[1] = greennew;
+            blend.at<Vec3b>(y,x)[2] = rednew;
+        }
+    }
+    ui->imageLabel3->setPixmap(cvMatToQPixmap(rescale(blend)));
+}
+
+void MainWindow::on_horizontalSlider_2_valueChanged(int value)
+{
+    Mat binary(picture.rows, picture.cols, CV_THRESH_BINARY);
+    for(int x = 0; x < picture.cols; x++){
+        for(int y = 0; y < picture.rows; y++){
+            double red = picture.at<Vec3b>(y,x)[2];
+            double green = picture.at<Vec3b>(y,x)[1];
+            double blue = picture.at<Vec3b>(y,x)[0];
+            if(((red + green + blue) /3) >= value){
+                binary.at<uchar>(y,x) = 255;
+            }
+            else{
+                binary.at<uchar>(y,x) = 0;
+            }
+        }
+    }
+    ui->imageLabel3->setPixmap(cvMatToQPixmap(rescale(binary)));
+}
+
+void MainWindow::on_horizontalSlider_4_valueChanged(int value)
+{
+    shift = value;
+    Mat contrast;
+    picture.copyTo(contrast);
+    for(int x = 0; x < picture.cols; x++){
+        for(int y = 0; y < picture.rows; y++){
+            contrast.at<Vec3b>(y,x)[0] = picture.at<Vec3b>(y,x)[0] * scale + shift;
+            contrast.at<Vec3b>(y,x)[1] = picture.at<Vec3b>(y,x)[1] * scale + shift;
+            contrast.at<Vec3b>(y,x)[2] = picture.at<Vec3b>(y,x)[2] * scale + shift;
+
+        }
+    }
+    ui->imageLabel2->setPixmap(cvMatToQPixmap(rescale(contrast)));
 }
--- Aufgaben2/mainwindow.h
+++ Aufgaben2/mainwindow.h
@@ -18,7 +18,20 @@ public:
 private slots:
 
     void on_action_ffnen_triggered();
+    void on_actionZweites_Bild_triggered();
+    void on_actionHelligkeit_Kontrast_triggered();
+    void on_actionGraustufen_Bin_r_triggered();
+    void on_actionRGB_Komponenten_triggered();
+    void on_actionBlendfunktion_triggered();
 
+
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_horizontalSlider_3_valueChanged(int value);
+
+    void on_horizontalSlider_2_valueChanged(int value);
+
+    void on_horizontalSlider_4_valueChanged(int value);
 
 private:
     cv::Mat MainWindow::rescale(cv::Mat &mat);
